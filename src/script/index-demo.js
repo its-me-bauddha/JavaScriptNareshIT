25af52b15c7cc73caf090ecd6f8d189bdeed65aa
its-me-bauddha <harshbauddha563@gmail.com>
2023-08-28 21:27:26 +0530

    lecture 8  ,9 and 10

---
+++ src/script/index-demo.js
@@ -0,0 +1,5 @@
+
+// function RegisterClick(){
+//             document.getElementById("DetailsContainer").style.display= "block";
+//             //document,getElementById("RegisterContainer").style.display
+// }

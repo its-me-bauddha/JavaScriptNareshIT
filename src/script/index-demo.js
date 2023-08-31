6050742d42107f5c0b41ef7d0dafdc9a3afcaf64
its-me-bauddha <harshbauddha563@gmail.com>
2023-08-31 16:08:49 +0530

    object

--- src/script/index-demo.js
+++
@@ -1,5 +0,0 @@
-
-// function RegisterClick(){
-//             document.getElementById("DetailsContainer").style.display= "block";
-//             //document,getElementById("RegisterContainer").style.display
-// }

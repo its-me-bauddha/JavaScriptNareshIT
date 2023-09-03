c8cde18f99c8bdc11a6a62f3cd59f07963282367
its-me-bauddha <harshbauddha563@gmail.com>
2023-09-03 08:13:01 +0530

    code

---
+++ library/modules/home-module.js
@@ -0,0 +1,11 @@
+
+var userName ="John";
+
+export function printName(){
+    return `Hello ! ${userName}`;
+}
+
+
+export function Addition(a,b){
+    return a + b;
+}
---
+++ library/modules/product-module.js
@@ -0,0 +1,9 @@
+
+
+export function GetProduct(){
+
+    return[
+        {Name :"Tv" ,Price :23424.34},
+        {Name :"Samsung", Price:232133},
+    ]
+}
